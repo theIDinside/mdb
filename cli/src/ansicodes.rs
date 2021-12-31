71ff792a8d222c92facbbe0f9d01ee9b6ed148a4
Simon Farre <simon.farre.cx@gmail.com>
2021-12-31 15:46:55 +0100

    disable warnings for unused ansi codes for now

--- cli/src/ansicodes.rs
+++ cli/src/ansicodes.rs
@@ -1,11 +1,12 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
 
 pub const CLEAR_SCREEN: &'static str = "\x1b[2J";
 
 pub const CLEAR_LINE_FROM_CURSOR: &'static str = "\x1b[0K";
-pub const CLEAR_LINE_TO_CURSOR: &'static str = "\x1b[1K";
+
 pub const CLEAR_LINE: &'static str = "\x1b[2K";
 
+// currently unused
 pub const SET_BOLD: &'static str = "\x1b[1m";
 pub const SET_DIM: &'static str = "\x1b[2m";
 pub const COLOR_BLACK: (&'static str, &'static str) = ("30", "40");
@@ -14,5 +15,5 @@ pub const COLOR_GREEN: (&str, &str) = ("32", "42");
 pub const COLOR_WHITE: (&str, &str) = ("37", "37");
 pub const RESET: &str = "0";
 pub const LAST_LINE: &str = "\x1b[99999B";
-
 pub const RESET_PROMPT: &str = "\x1b[9999B\x1b[2Jmdb>";
+pub const CLEAR_LINE_TO_CURSOR: &'static str = "\x1b[1K";
