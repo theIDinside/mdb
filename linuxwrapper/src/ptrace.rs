41dda3e67679116dac7fb7cb2e1f1ffc5d17ca4a
Simon Farre <simon.farre.cx@gmail.com>
2022-01-01 14:59:34 +0100

    move all ptrace related stuff that we use, to own module

---
+++ linuxwrapper/src/ptrace.rs
@@ -0,0 +1,23 @@
+use crate::Pid;
+pub(in crate) use libc::ptrace;
+
+/** Kills the target debuggee if Midas debugger exits
+ * pid - the process id which this should be true for
+ * returns - foo
+*/
+pub fn kill_on_midas_exit(pid: Pid) -> Result<(), String> {
+    use libc::PTRACE_SETOPTIONS;
+    unsafe {
+        if ptrace(
+            PTRACE_SETOPTIONS,
+            *pid,
+            std::ptr::null() as *const libc::c_void,
+            std::ptr::null() as *const libc::c_void,
+        ) == -1
+        {
+            Err(crate::errno::get_errno_msg())
+        } else {
+            Ok(())
+        }
+    }
+}
