1b26b98197606fd81bbedc90164da6333db48833
Simon Farre <simon.farre.cx@gmail.com>
2022-01-01 19:56:43 +0100

    removed auto impl on traits, since that defeats the purpose of the trait in the first place

--- midas/src/target/mod.rs
+++ midas/src/target/mod.rs
@@ -1,29 +1,18 @@
-use libc::{ptrace, PTRACE_SETOPTIONS};
-use nixwrap::{get_errno_msg, unwrap_err_err, Pid, WaitStatus};
+use nixwrap::{Pid, WaitStatus};
 pub mod linux;
 
-use crate::MidasSysResult;
+use nixwrap::MidasSysResult;
 
 // represents the state operations we can do on the debuggeee
 pub trait Target {
+    type OSTarget;
+    fn launch(
+        path: &std::path::Path,
+    ) -> MidasSysResult<(Box<<Self as Target>::OSTarget>, WaitStatus)>;
     fn process_id(&self) -> Pid;
     fn step(&self, steps: usize);
-    fn exec_continue(&self);
+    fn continue_execution(&self) -> MidasSysResult<WaitStatus>;
     fn kill(&self) -> MidasSysResult<WaitStatus>;
     fn read_memory(&self, address: usize, bytes: usize) -> Vec<u8>;
-    fn kill_on_tracer_exit(&self) -> MidasSysResult<()> {
-        unsafe {
-            if ptrace(
-                PTRACE_SETOPTIONS,
-                *self.process_id(),
-                std::ptr::null() as *const libc::c_void,
-                std::ptr::null() as *const libc::c_void,
-            ) == -1
-            {
-                Err(unwrap_err_err(get_errno_msg()))
-            } else {
-                Ok(())
-            }
-        }
-    }
+    fn kill_on_tracer_exit(&self) -> MidasSysResult<()>;
 }
