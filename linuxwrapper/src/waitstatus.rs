cef8538188ecb92e647e408941d223f8e1554007
Simon Farre <simon.farre.cx@gmail.com>
2022-01-01 22:34:20 +0100

    removed prior definition of Signal and moved it to nixwrap

--- linuxwrapper/src/waitstatus.rs
+++ linuxwrapper/src/waitstatus.rs
@@ -1,4 +1,6 @@
 use std::ops::Deref;
+
+use crate::signals::Signal;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct Pid(pub libc::pid_t);
 
@@ -6,14 +8,6 @@ impl Deref for Pid {
     type Target = libc::pid_t;
     fn deref(&self) -> &Self::Target {
         &self.0
-    }
-}
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
-pub struct Signal(pub i32);
-
-impl Signal {
-    pub fn from(num: i32) -> Self {
-        Self(num)
     }
 }
 
@@ -24,7 +18,7 @@ pub enum WaitStatus {
     /** WIFEXITED */
     ExitedNormally(Pid, i32),
     /** WIFSTOPPED */
-    Stopped(Pid),
+    Stopped(Pid, Signal),
     /** WIFSIGNALLED */
     Killed(Pid, Signal),
     /** WCOREDUMP */
@@ -42,9 +36,12 @@ impl WaitStatus {
                 WEXITSTATUS(wait_status_raw_value),
             ))
         } else if WIFSTOPPED(wait_status_raw_value) {
-            Ok(WaitStatus::Stopped(pid))
+            Ok(WaitStatus::Stopped(
+                pid,
+                Signal::from_raw(libc::WSTOPSIG(wait_status_raw_value)).unwrap(),
+            ))
         } else if WIFSIGNALED(wait_status_raw_value) {
-            let signal = Signal::from(libc::WTERMSIG(wait_status_raw_value));
+            let signal = Signal::from_raw(libc::WTERMSIG(wait_status_raw_value)).unwrap();
             if WCOREDUMP(wait_status_raw_value) {
                 Ok(WaitStatus::CoreDumped(pid))
             } else {
