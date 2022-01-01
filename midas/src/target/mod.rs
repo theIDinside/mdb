e7f8783e50682e45c9f9ba99691a3d2b844d99f8
Simon Farre <simon.farre.cx@gmail.com>
2022-01-01 15:02:25 +0100

    Target trait, that I suppose one day could be extended to map over different OS's

--- midas/src/target/mod.rs
+++ midas/src/target/mod.rs
@@ -1,9 +1,8 @@
-use nixwrap::WaitStatus;
+use libc::{ptrace, PTRACE_SETOPTIONS};
+use nixwrap::{get_errno_msg, unwrap_err_err, Pid, WaitStatus};
+pub mod linux;
 
 use crate::MidasSysResult;
-
-#[cfg(target_os = "linux")]
-pub struct Pid(libc::pid_t);
 
 // represents the state operations we can do on the debuggeee
 pub trait Target {
@@ -11,4 +10,20 @@ pub trait Target {
     fn step(&self, steps: usize);
     fn exec_continue(&self);
     fn kill(&self) -> MidasSysResult<WaitStatus>;
+    fn read_memory(&self, address: usize, bytes: usize) -> Vec<u8>;
+    fn kill_on_tracer_exit(&self) -> MidasSysResult<()> {
+        unsafe {
+            if ptrace(
+                PTRACE_SETOPTIONS,
+                *self.process_id(),
+                std::ptr::null() as *const libc::c_void,
+                std::ptr::null() as *const libc::c_void,
+            ) == -1
+            {
+                Err(unwrap_err_err(get_errno_msg()))
+            } else {
+                Ok(())
+            }
+        }
+    }
 }
