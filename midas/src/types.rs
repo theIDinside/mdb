48e8672e23e922ac37290e2c25b4996bc9fb6ed8
Simon Farre <simon.farre.cx@gmail.com>
2022-01-02 01:22:39 +0100

    added displaying of the Address type (which is a tuple, wrapped around an usize)

--- midas/src/types.rs
+++ midas/src/types.rs
@@ -1,2 +1,25 @@
+use std::fmt;
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Address(usize);
+pub struct Address(pub usize);
+
+impl Address {
+    #[inline]
+    pub fn value(&self) -> usize {
+        self.0
+    }
+}
+
+impl fmt::LowerHex for Address {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let val = self.0;
+        fmt::LowerHex::fmt(&val, f) // delegate to i32's implementation
+    }
+}
+
+impl fmt::UpperHex for Address {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let val = self.0;
+        fmt::UpperHex::fmt(&val, f) // delegate to i32's implementation
+    }
+}
