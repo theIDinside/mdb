e7c34568061c8fce080e4b1c233d94140ed4736f
Simon Farre <simon.farre.cx@gmail.com>
2022-01-03 19:46:34 +0100

    added midas_err utility function that maps Result<T>::Error<U> to Result<T>::Error<String> basically.
    It shortens what we need to write. We could utilize macros here, but I honestly don't think that's a pragmatic use case / best praxis for macros.

--- midas/src/utils.rs
+++ midas/src/utils.rs
@@ -1,1 +1,6 @@
-
+/// Takes a to_string'able type and to_string's it. Utility function, for shortening the code
+/// that for instance, returns a Result<T, SomeErrorThatIsNotCompatibleWithMidasSysResult::Error> and converts it to our Error format (which currently is a String only, but in the future will change)
+#[inline(always)]
+pub fn midas_err<T: ToString>(to_stringable_error: T) -> String {
+    to_stringable_error.to_string()
+}
