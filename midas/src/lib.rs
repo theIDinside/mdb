ce4fd92b798704512897ccd7a56b62a3ccacc2ef
Simon Farre <simon.farre.cx@gmail.com>
2022-01-06 19:51:30 +0100

    Added the non-allocating Result type MidasSysResult

--- midas/src/lib.rs
+++ midas/src/lib.rs
@@ -9,5 +9,27 @@ pub mod target;
 pub mod types;
 pub mod utils;
 
+pub enum MidasError {
+    BadUnsignedLEB128Encoding(usize),
+    BadSignedLEB128Encoding(usize),
+}
+
+impl MidasError {
+    pub fn description(&self) -> &'static str {
+        match self {
+            MidasError::BadUnsignedLEB128Encoding(_) => "[Bad format]: Decoding unsigned LEB128 failed",
+            MidasError::BadSignedLEB128Encoding(_) => "[Bad format]: Decoding signed LEB128 failed",
+        }
+    }
+}
+
+impl std::fmt::Display for MidasError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.description())
+    }
+}
+
+pub type MidasSysResult<T> = Result<T, MidasError>;
+
 #[cfg(test)]
 mod tests {}
