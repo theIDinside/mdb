002c55b3b3f2eed6ad343530215f00df7a043d4b
Simon Farre <simon.farre.cx@gmail.com>
2022-01-16 12:42:37 +0100

    BasicTypeTuple macro; creates wrappers around types and auto-impls things we need for them. This is to signal intent more clearly

--- midas/src/types.rs
+++ midas/src/types.rs
@@ -1,4 +1,46 @@
-use std::fmt;
+use std::{
+    fmt,
+    ops::{Deref, DerefMut},
+};
+
+// Macro that wraps types, for clarity in the API what they are meant to represent.
+macro_rules! BasicTypeTuple {
+    ($struct_name:ident($wrapped_type:ty)) => {
+        #[derive(Debug, PartialEq, PartialOrd, Ord, Eq, Clone, Copy)]
+        pub struct $struct_name(pub $wrapped_type);
+
+        impl AsRef<$wrapped_type> for $struct_name {
+            fn as_ref(&self) -> &usize {
+                &self.0
+            }
+        }
+
+        impl AsMut<$wrapped_type> for $struct_name {
+            fn as_mut(&mut self) -> &mut $wrapped_type {
+                &mut self.0
+            }
+        }
+
+        impl Deref for $struct_name {
+            type Target = $wrapped_type;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl DerefMut for $struct_name {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+
+        impl Into<$struct_name> for $wrapped_type {
+            fn into(self) -> $struct_name {
+                $struct_name(self)
+            }
+        }
+    };
+}
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub struct Address(pub usize);
@@ -34,5 +76,33 @@ impl fmt::UpperHex for Address {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let val = self.0;
         fmt::UpperHex::fmt(&val, f) // delegate to i32's implementation
+    }
+}
+
+BasicTypeTuple!(Index(usize));
+
+#[derive(Debug)]
+pub struct SectionPointer {
+    ptr: *const u8,
+    size: usize,
+}
+
+impl SectionPointer {
+    pub unsafe fn from_raw(ptr: *const u8, size: usize) -> SectionPointer {
+        SectionPointer { ptr, size }
+    }
+
+    pub fn from_slice(slice: &[u8]) -> SectionPointer {
+        let ptr = slice.as_ptr();
+        let size = slice.len();
+        SectionPointer { ptr, size }
+    }
+
+    pub fn data(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.ptr, self.size) }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
     }
 }
